6e6ef855cff4b825e78e4ecc36192a48c59c6324
Izzy Lancaster <avrisaac555@gmail.com>
2022-07-05 23:19:49 -0700

    For SQL resaons, decided against using framework

--- discord-bot/src/main.rs
+++ discord-bot/src/main.rs
@@ -1,28 +1,22 @@
 use std::env;
 
-use serenity::framework::StandardFramework;
-use serenity::framework::standard::CommandResult;
-use serenity::framework::standard::macros::{group, command};
-use serenity::http::CacheHttp;
 use serenity::model::channel::Message;
 use serenity::{ Client, async_trait, model::gateway::Ready};
 use serenity::prelude::GatewayIntents;
 use serenity::client::{EventHandler, Context};
 
-#[group]
-#[commands(ping)]
-struct General;
-
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.channel_id.say(&ctx.http, "Pong!").await?;
-    Ok(())
-}
-
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "~ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -35,13 +29,8 @@ async fn main() {
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~"))
-        .group(&GENERAL_GROUP);
-
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
-        .framework(framework)
         .await
         .expect("Error created client");
     
