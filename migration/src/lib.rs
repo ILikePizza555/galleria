755b3d9bca4f191972516e44b95db15114a36798
Izzy Lancaster <avrisaac555@gmail.com>
2022-07-15 20:32:30 -0700

    Update migration\lib.rs

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,9 +1,6 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
-mod m20220710_122414_rename_channel_id;
-mod m20220710_125055_discord_ids_unique_indecies;
-
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -11,8 +8,6 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20220710_122414_rename_channel_id::Migration),
-            Box::new(m20220710_125055_discord_ids_unique_indecies::Migration),
         ]
     }
 }
