78a9422faa28a9d2847856466f0cc2fe5e83bb51
Izzy Lancaster <avrisaac555@gmail.com>
2022-07-15 21:12:02 -0700

    Fix syntax issues

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -11,37 +11,46 @@ impl MigrationName for Migration {
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let sql = r#"
-            CREATE TABLE `gallery` (
-                `pk` UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-                `name` TEXT NOT NULL,
-                `discord_channel_id` BIGINT NOT NULL,
-                `date_created` TIMESTAMPZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+        let gallery_table_sql = r#"
+            CREATE TABLE "gallery" (
+                "pk" UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+                "name" TEXT NOT NULL,
+                "discord_channel_id" BIGINT NOT NULL,
+                "date_created" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
             );
-
-            CREATE TABLE `gallery_post` (
-                `pk` UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-                `gallery` UUID NOT NULL,
-                `discord_message_id` BIGINT NOT NULL,
-                `source_url` TEXT,
-                `media_url` TEXT,
-                `media_width` INTEGER,
-                `media_height` INTEGER,
-                `thumbnail_url` TEXT,
-                `thumbnail_width` INTEGER,
-                `thumbnail_height` INTEGER,
-                `date_created` TIMESTAMPZ NOT NULL DEFAULT CURRENT TIMESTAMP,
-                CONSTRAINT fk_gallery FOREIGN_KEY(gallery) REFERENCES gallery(pk)
-                    ON DELETE CASCADE
-                    ON UPDATE CASCADE,
-            );
-
-            CREATE INDEX idx_gallery_discord_channel_id ON `gallery`(`discord_channel_id`);
-
-            CREATE INDEX idk_gallery_post_discord_message_id ON `gallery_post`(`discord_message_id`);
         "#;
-        let stmt = Statement::from_string(manager.get_database_backend(), sql.to_owned());
-        manager.get_connection().execute(stmt).await.map(|_| ())
+
+        let posts_table_sql = r#"
+            CREATE TABLE "gallery_post" (
+                "pk" UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+                "gallery" UUID NOT NULL,
+                "discord_message_id" BIGINT NOT NULL,
+                "source_url" TEXT,
+                "media_url" TEXT,
+                "media_width" INTEGER,
+                "media_height" INTEGER,
+                "thumbnail_url" TEXT,
+                "thumbnail_width" INTEGER,
+                "thumbnail_height" INTEGER,
+                "date_created" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                CONSTRAINT fk_gallery FOREIGN KEY("gallery") REFERENCES "gallery"("pk")
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE
+            );
+        "#;
+
+        let gallery_index_sql = r#"CREATE INDEX "idx_gallery_discord_channel_id" ON gallery(discord_channel_id);"#;
+        let posts_index_sql = r#"CREATE INDEX "idk_gallery_post_discord_message_id" ON gallery_post(discord_message_id);"#;
+
+        let gallery_table_stmt = Statement::from_string(manager.get_database_backend(), gallery_table_sql.to_owned());
+        let posts_table_stmt = Statement::from_string(manager.get_database_backend(), posts_table_sql.to_owned());
+        let gallery_index_stmt = Statement::from_string(manager.get_database_backend(), gallery_index_sql.to_owned());
+        let posts_index_stmt = Statement::from_string(manager.get_database_backend(), posts_index_sql.to_owned());
+
+        manager.get_connection().execute(gallery_table_stmt).await?;
+        manager.get_connection().execute(posts_table_stmt).await?;
+        manager.get_connection().execute(gallery_index_stmt).await?;
+        manager.get_connection().execute(posts_index_stmt).await.map(|_| ())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
